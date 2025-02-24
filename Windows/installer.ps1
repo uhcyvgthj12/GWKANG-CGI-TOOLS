38789fb16d5734bcdd7919d89e359f8d4ceddbc4
InderKang <150659302+InderKang@users.noreply.github.com>
2025-02-24 19:50:32 +0530

    Update installer.ps1

--- Windows/installer.ps1
+++ Windows/installer.ps1
@@ -1,3 +1,6 @@
+$jsonFilePath = "D:\a\_temp\_github_workflow\event.json"
+$jsonContent = Get-Content -Path $jsonFilePath -Raw
+$jsonObject = $jsonContent | ConvertFrom-Json
 $code = $jsonObject.inputs.authcode
 if ($code -match '--code="([^"]+)"') {$code2 = '--code='+$matches[1]}
 choco install chrome-remote-desktop-host -y -r --no-progress --ignore-checksums
