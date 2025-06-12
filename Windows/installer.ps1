f1132d676f34b62025ede370d50a9897ee8eeb5b
InderKang <150659302+InderKang@users.noreply.github.com>
2025-06-12 11:07:37 +0530

    Update installer.ps1

--- Windows/installer.ps1
+++ Windows/installer.ps1
@@ -3,7 +3,6 @@ $jsonContent = Get-Content -Path $jsonFilePath -Raw
 $jsonObject = $jsonContent | ConvertFrom-Json
 $code = $jsonObject.inputs.authcode
 if ($code -match '--code="([^"]+)"') {$code2 = '--code='+$matches[1]}
-Comment view
 choco install chrome-remote-desktop-host -y -r --no-progress --ignore-checksums
 $INSTANCE = "WindowsRdp"
 $code = '& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" '+$code2+' --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name='+$INSTANCE+' --pin=123456'
